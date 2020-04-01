46a451029c184e90e900170d16d6308f573e9f1a
Fredrik Engstrand <frewacom@gmail.com>
2020-04-01 17:40:26 +0200

    Added build and debug scripts. Changed project structure

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+  devtool: process.env.NODE_ENV === 'development' ? 'eval' : 'source-map',
+
+  entry: {
+    background: path.resolve(__dirname, 'src/background/index.ts'),
+    inject: path.resolve(__dirname, 'src/inject/index.ts')
+  },
+  output: {
+    path: path.resolve(__dirname, 'extension/dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loaders: [ 'html-loader' ]
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  }
+}
