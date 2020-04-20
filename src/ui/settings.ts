0a7a4bd2b95d4d1604eab6ce27303ac262b3db3b
Fredrik Engstrand <frewacom@gmail.com>
2020-04-20 16:30:18 +0200

    Added icon classes. Setting menus can be opened/closed

--- src/ui/settings.ts
+++ src/ui/settings.ts
@@ -1,1 +1,11 @@
+const settingCardHeaders = Array.from(document.getElementsByClassName('setting-card-header'));
 
+function onSettingCardClick(header) {
+  const card = header.parentNode;
+  const isOpen = card.getAttribute('open');
+  isOpen ? card.removeAttribute('open') : card.setAttribute('open', true);
+}
+
+settingCardHeaders.forEach((header) => {
+  header.addEventListener('click', () => onSettingCardClick(header));
+});
